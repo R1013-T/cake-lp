ab2214aa74ea41373dfe14bd031bb8184e358e9d
R1013-T <nosuke1013@icloud.com>
2023-06-18 00:03:46 +0900

    change lang active weight

--- src/components/main/Lang.tsx
+++ src/components/main/Lang.tsx
@@ -7,14 +7,18 @@ const Lang = ({ lang, changeLang }: Props) => {
   return (
     <div className="fixed right-0 top-0 z-10 flex h-10 items-center justify-between px-5 font-body font-light text-bk transition">
       <p
-        className={lang === 0 ? "text-blue opacity-100" : "opacity-40"}
+        className={
+          lang === 0 ? "font-medium text-blue opacity-100" : "opacity-40"
+        }
         onClick={() => changeLang(0)}
       >
         JP
       </p>
       <p className="px-1 opacity-60">/</p>
       <p
-        className={lang === 1 ? "text-blue opacity-100" : "opacity-40"}
+        className={
+          lang === 1 ? "font-medium text-blue opacity-100" : "opacity-40"
+        }
         onClick={() => changeLang(1)}
       >
         EN
